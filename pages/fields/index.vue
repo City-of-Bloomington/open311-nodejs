8debb4ea80c11f66d38a03b7ef18a5aadaacc6f8
adabutch <adabutch1@gmail.com>
2018-09-14 12:52:39 -0400

    process data for blob if there is data

--- pages/fields/index.vue
+++ pages/fields/index.vue
@@ -230,24 +230,25 @@ export default {
       this.top = this.$refs.top.clientHeight + 'px';
     },
     dataURItoBlob(dataURI) {
-      alert(dataURI);
-      // convert base64/URLEncoded data component to raw binary data held in a string
-      var byteString;
-      if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-      else
-        byteString = unescape(dataURI.split(',')[1]);
+      if(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+          byteString = atob(dataURI.split(',')[1]);
+        else
+          byteString = unescape(dataURI.split(',')[1]);
 
-      // separate out the mime component
-      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-      // write the bytes of the string to a typed array
-      var ia = new Uint8Array(byteString.length);
-      for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], {type:mimeString});
       }
-
-      return new Blob([ia], {type:mimeString});
     },
     capture() {
       this.canvas = this.$refs.canvas;
