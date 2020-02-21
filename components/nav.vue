0d92ffb4d244caf44b42d821c0e6b8c7a1a28cf4
adabutch <adabutch1@gmail.com>
2020-02-21 10:20:49 -0500

    fix: #51 - clickable step two
    We need to check if the step is two, bc our router chokes on replacing the same string.

--- components/nav.vue
+++ components/nav.vue
@@ -1,45 +1,5 @@
 <template>
   <div>
-    <!-- <div class="progress-stepper">
-      <span class="title">Progress:</span>
-      <div v-bind:class="{ active: stepActive.one, 'complete': stepComplete.one }">
-        <svg v-if="stepComplete.one" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.one">1</span>
-      </div>
-      <div v-bind:class="{ active: stepActive.two, 'complete': stepComplete.two }">
-        <svg v-if="stepComplete.two" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.two">2</span>
-      </div>
-      <div v-bind:class="{ active: stepActive.three, 'complete': stepComplete.three }">
-        <svg v-if="stepComplete.three" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.three">3</span>
-      </div>
-      <div v-bind:class="{ active: stepActive.four, 'complete': stepComplete.four }">
-        <svg v-if="stepComplete.four" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.four">4</span>
-      </div>
-      <div v-bind:class="{ active: stepActive.five, 'complete': stepComplete.five }">
-        <svg v-if="stepComplete.five" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.five">5</span>
-      </div>
-      <div v-bind:class="{ active: stepActive.six, 'complete': stepComplete.six }">
-        <svg v-if="stepComplete.six" version="1.1" id="check" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="46px" height="37px" viewBox="0 0 46 37" enable-background="new 0 0 46 37" xml:space="preserve">
-          <polygon id="check-icon" fill="" points="0.2230862,22.3627338 14.1432562,36.2812843 45,5.4261613 40.3345947,0.7607598 14.1432562,26.9504795 4.7655683,17.5744114 "/>
-        </svg>
-        <span v-if="!stepComplete.six">6</span>
-      </div>
-    </div> -->
-
     <div class="progress-stepper">
       <span class="title">Progress:</span>
       
@@ -269,8 +229,8 @@ export default {
     },
     goToStep(step) {
       if(step <= this.stepActive){
-        if(step == 2) {
-          this.$router.push('./')
+        if(step == 2){
+          this.$router.push(`/${this.stepperPaths[step].path}`)
         } else {
           this.$router.push(this.stepperPaths[step].path)
         }
