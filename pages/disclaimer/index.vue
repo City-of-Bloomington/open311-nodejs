9d7ce6acd508ad85248092930d3324a919eadc0f
adabutch <adabutch1@gmail.com>
2020-03-17 13:37:29 -0400

    style: help/disclaimer page

--- pages/disclaimer/index.vue
+++ pages/disclaimer/index.vue
@@ -4,17 +4,19 @@
       <div class="container">
         <h1>Notice:</h1>
 
-        <div>
+        <!-- <div>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.994 14.994"><title>close</title><g id="close-icon" data-name="close-icon"><line x1="14.287" y1="0.707" x2="0.707" y2="14.287" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="2"/><line x1="0.707" y1="0.707" x2="14.287" y2="14.287" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="2"/></g></svg>
-        </div>
+        </div> -->
       </div>
     </div>
 
     <main>
       <h2>Non-Emergency Use Only</h2>
-      <p><strong><i>This system is not for reporting emergencies or imminent safety hazards.</i></strong></p>
+      <p><strong>If this is an emergency, please dial 911.</strong></p>
 
-      <p>If this is an emergency, please dial 911.</p>
+
+      <p>This system is not for reporting emergencies or imminent safety hazards.</p>
+
       
       <p>If this is a safety hazard that could cause an injury, please immediately call <strong>City Utilities Control at 812-339-1444</strong>.</p>
 
@@ -28,90 +30,60 @@
       <p>Information you provide through uReport is stored and retained by the City as a public record.</p>
 
       <h2>Tickets</h2>
-      <p>Tickets may be viewed at:<br>
-      <strong><a :href="crmUrl" target="_blank" title="View uRport CRM tickets">{{ crmUrl }}</a></strong></p>
+      <p>Tickets may be viewed by searching for your ticket number on the homepage, or by visiting:<br>
+
+        <strong>
+          <a
+            :href="crmUrl"
+            target="_blank"
+            title="View uRport CRM tickets">
+            {{ crmUrl }}
+          </a>
+        </strong>
+      </p>
     </main>
   </div>
 </template>
 
 <style lang="scss">
 .disclaimer {
-  background: $color-blue;
-  color: white;
-  width: 100%;
-  // position: fixed;
-  // top: 0;
-  // left: 0;
-  // right: 0;
-  // height: 100%;
-  overflow: scroll;
-  
-  .top {
-    display: flex;
-    justify-content: center;
-    padding: 0;
-  }
-
-  .container {
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    margin: 10px auto 0 auto;
-    padding: 0 0 10px 0;
-    max-width: 520px;
-    width: 100%;
-    height: 100%;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
-
-    div {
-      &:first-of-type {
-        margin-left: auto;
-      }
-    }
-  }
-
-  main {
-    a {
-      width: auto !important;
-      height: auto !important;
-      margin: 0 !important;
-    }
-  }
-
-  h1, h2 {
-    font-weight: 600;
-  }
+  position: relative;
+  margin: 0 auto;
+  padding: 20px 0;
+  max-width: 520px;
 
   h1 {
-    margin: 0;
+    color: white;
+    font-size: 28px;
+    font-weight: 600;
+    letter-spacing: .5px;
+    display: inline-block;
+    line-height: 30px;
+    margin: 0 0 15px 0;
   }
 
   h2 {
-    margin: 40px 0 5px 0;
-
-    &:first-of-type {
-      margin: 20px 0 5px 0;
-    }
+    color: white;
+    font-size: 20px;
+    line-height: 22px;
+    font-weight: 600;
+    letter-spacing: .25px;
+    margin: 0 0 5px 0;
   }
 
   p {
-    margin: 0 0 20px 0;
+    color: white;
     font-size: 18px;
     letter-spacing: .5px;
-    line-height: 25px;
+    position: relative;
+    width: 100%;
+    margin: 0 0 20px 0;
+    overflow: hidden;
 
-    &:last-child {
-      margin: 0;
+    a {
+      color: white;
+      text-decoration: underline;
     }
-  }
-
-  svg {
-    display: block;
-    width: 20px;
-    height: 20px;
-    margin-left: auto;
-    cursor: pointer;
   }
 }
 </style>
