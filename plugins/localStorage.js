be1b30af7b193416456f6740cc7e4b4d5485b185
adabutch <adabutch1@gmail.com>
2021-06-03 11:44:41 -0400

    update: cache - don't catch services

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,13 +1,25 @@
-import createPersistedState from 'vuex-persistedstate';
+import createPersistedState from "vuex-persistedstate";
 
 export default ({ store, isHMR }) => {
   if (isHMR) return;
 
   if (process.client) {
-    window.onNuxtReady((nuxt) => {
+    window.onNuxtReady(nuxt => {
       createPersistedState({
         reducer(state, paths) {
           const reducer = Object.assign({}, state);
+          delete reducer.initGroupData;
+          delete reducer.CITY_NAME;
+          delete reducer.cityBoundary;
+          delete reducer.cityHallLatLong;
+          delete reducer.coordsProjection;
+          delete reducer.fromServiceCode;
+          delete reducer.routeNames;
+          delete reducer.serviceTicketCRMHTML;
+          delete reducer.serviceTicketData;
+          delete reducer.serviceTypes;
+          delete reducer.stepperPaths;
+          delete reducer.consoleLog;
           delete reducer.totalSteps;
           delete reducer.seen_modal;
           delete reducer.topbar_height;
@@ -17,7 +29,7 @@ export default ({ store, isHMR }) => {
           delete reducer.loc_search_height;
           return reducer;
         }
-      })(store)
+      })(store);
     });
   }
-}
+};
--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const {
-  Nuxt,
-  Builder }       = require('nuxt'),
-  config          = require('./nuxt.config.js'),
-  pkg             = require('./package'),
-  https           = require('https'),
-  fs              = require('fs'),
-  nuxt            = new Nuxt(config),
+const { Nuxt, Builder } = require("nuxt"),
+  config = require("./nuxt.config.js"),
+  pkg = require("./package"),
+  https = require("https"),
+  fs = require("fs"),
+  nuxt = new Nuxt(config),
   options = {
-                  key: fs.readFileSync('certs/server.key', 'utf8'),
-                  cert: fs.readFileSync('certs/wildcard.bloomington.in.gov-fullChain.crt', 'utf8')
+    key: fs.readFileSync("./certs/STAR_bloomington_in_gov.key", "utf8"),
+    cert: fs.readFileSync(
+      "./certs/STAR_bloomington_in_gov-fullchain.crt",
+      "utf8"
+    )
   },
-  port            = process.env.LOCAL_PORT || 2222,
-  isProd          = (process.env.NODE_ENV === 'production'),
-  envMsg          = config.dev ? 'Development' : 'Production',
-  dividerStars    = `★\xa0\xa0`,
+  port = process.env.LOCAL_PORT || 2222,
+  isProd = process.env.NODE_ENV === "production",
+  envMsg = config.dev ? "Development" : "Production",
+  dividerStars = `★\xa0\xa0`,
   starRepeatCount = 22,
-  devUrl          = `https://dhcp-cityhall-xxx-xxx.bloomington.in.gov`,
-  dividerMsg      = `🛠️\xa0\xa0⛓️\xa0\xa0👩‍💻\xa0🔮\xa0👨‍💻\xa0⛓️\xa0\xa0🛠️`;
+  devUrl = `https://dhcp-cityhall-xxx-xxx.bloomington.in.gov`,
+  dividerMsg = `🛠️\xa0\xa0⛓️\xa0\xa0👩‍💻\xa0🔮\xa0👨‍💻\xa0⛓️\xa0\xa0🛠️`;
 
-config.dev    = !isProd;
+config.dev = !isProd;
 
 // Build only in dev mode with hot-reloading
 if (config.dev) {
-  new Builder(nuxt).build()
-  .then(listen)
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
-} else { listen() }
+  new Builder(nuxt)
+    .build()
+    .then(listen)
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+} else {
+  listen();
+}
 
 function listen() {
-  https
-  .createServer(options, nuxt.render)
-  .listen(port);
+  https.createServer(options, nuxt.render).listen(port);
 
-  console.log(`\n\n${dividerMsg}\n`
-            + `${dividerStars.repeat(starRepeatCount)}\n`
-            + `App:  ${pkg.prettyName}\n`
-            + `Env:  ${envMsg}\n`
-            + `Who:  ${pkg.company}\n`
-            + `Repo: ${pkg.repository.url}\n`
-            + `Url:  ${devUrl}:${port}`
-            + `\n${dividerStars.repeat(starRepeatCount)}\n`
-            + `${dividerMsg}\n\n`);
+  console.log(
+    `\n\n${dividerMsg}\n` +
+      `${dividerStars.repeat(starRepeatCount)}\n` +
+      `App:  ${pkg.prettyName}\n` +
+      `Env:  ${envMsg}\n` +
+      `Who:  ${pkg.company}\n` +
+      `Repo: ${pkg.repository.url}\n` +
+      `Url:  ${devUrl}:${port}` +
+      `\n${dividerStars.repeat(starRepeatCount)}\n` +
+      `${dividerMsg}\n\n`
+  );
 }
